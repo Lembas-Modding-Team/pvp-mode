0b7636f43211a32ac88e9da809a684cdc8ccf5d8
CraftedMods <craftedMods@t-online.de>
2018-05-10 13:29:03 +0200

    The default combat log handler now supports log file rotation

--- main/java/pvpmode/log/SimpleCombatLogHandler.java
+++ main/java/pvpmode/log/SimpleCombatLogHandler.java
@@ -1,57 +1,29 @@
 package pvpmode.log;
 
-import java.io.*;
-import java.nio.file.Path;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.DamageSource;
 
-public class SimpleCombatLogHandler implements CombatLogHandler
+public class SimpleCombatLogHandler extends AbstractFileCombatLogHandler
 {
     public static final String CONFIG_NAME = "default";
 
-    private PrintWriter stream;
-
-    @Override
-    public void init(Path pvpLoggingDir)
+    public SimpleCombatLogHandler()
     {
-        try
-        {
-            File logFile = new File (pvpLoggingDir.getParent ().toFile (), "pvpmode.log");
-
-            if (!logFile.exists ())
-            {
-                logFile.getParentFile ().mkdirs ();
-                logFile.createNewFile ();
-            }
-
-            stream = new PrintWriter (logFile);
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException ("The pvp log file couldn't be created or accessed", e);
-        }
-
+        super (CONFIG_NAME,"log");
     }
 
     @Override
     public void log(Date date, EntityPlayer attacker, EntityPlayer victim, float damageAmount,
                     DamageSource damageSource)
     {
-        stream.println (String.format (
+        writer.println (String.format (
                         "[%s] %s or an unit of this player initiated an attack against %s dealing %.2f HP damage of the type %s",
                         SimpleDateFormat.getDateTimeInstance ().format (date), attacker.getDisplayName (),
                         victim.getDisplayName (), damageAmount, damageSource.damageType));
-        stream.flush ();
-
-    }
-
-    @Override
-    public void cleanup()
-    {
-        stream.close ();
+        writer.flush ();
 
     }
 
