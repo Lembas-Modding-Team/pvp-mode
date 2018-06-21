6e3789664210b783899153488b581ea534a8ac95
CraftedMods <craftedMods@t-online.de>
2018-06-21 11:01:25 +0200

    Prepared 1.1.0-BETA.2

--- src/main/java/pvpmode/PvPMode.java
+++ src/main/java/pvpmode/PvPMode.java
@@ -16,7 +16,7 @@ import pvpmode.compatibility.modules.lotr.LOTRModCompatibilityModuleLoader;
 import pvpmode.log.*;
 import pvpmode.overrides.PvPOverrideManager;
 
-@Mod(modid = "pvp-mode", name = "PvP Mode", version = "1.1.0-BETA.1", acceptableRemoteVersions = "*")
+@Mod(modid = "pvp-mode", name = "PvP Mode", version = "1.1.0-BETA.2", acceptableRemoteVersions = "*")
 public class PvPMode
 {
     public static Configuration config;
