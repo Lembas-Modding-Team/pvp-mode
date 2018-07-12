b75f53befbd824b8fa7bbb927d2335fd3841d67c
CraftedMods <craftedMods@t-online.de>
2018-07-12 16:37:06 +0200

    Prepared 1.2.0-BETA.5

--- src/main/java/pvpmode/PvPMode.java
+++ src/main/java/pvpmode/PvPMode.java
@@ -18,7 +18,7 @@ import pvpmode.compatibility.modules.suffixForge.SuffixForgeCompatibilityModuleL
 import pvpmode.log.*;
 import pvpmode.overrides.PvPOverrideManager;
 
-@Mod(modid = "pvp-mode", name = "PvP Mode", version = "1.2.0-BETA.4", acceptableRemoteVersions = "*")
+@Mod(modid = "pvp-mode", name = "PvP Mode", version = "1.2.0-BETA.5", acceptableRemoteVersions = "*")
 public class PvPMode
 {
     public static Configuration config;
