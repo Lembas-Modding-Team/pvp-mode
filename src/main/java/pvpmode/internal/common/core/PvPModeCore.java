44dad391f756e4c62f6ac32df71fc3fe95b397ba
CraftedMods <33467489+CraftedMods@users.noreply.github.com>
2018-08-29 16:12:32 +0200

    Closed #240
    PR #241

--- src/main/java/pvpmode/internal/common/core/PvPModeCore.java
+++ src/main/java/pvpmode/internal/common/core/PvPModeCore.java
@@ -7,6 +7,7 @@ import cpw.mods.fml.relauncher.IFMLLoadingPlugin.*;
 
 @MCVersion(value = "1.7.10")
 @SortingIndex(value = 5000)
+@TransformerExclusions(value = "pvpmode.internal.common.core")
 public class PvPModeCore implements IFMLLoadingPlugin
 {
     
